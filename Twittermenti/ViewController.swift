416cddd09a7c57fc9f2cbde93260eec1602f2570
Cody Crawmer <codercodyc@gmail.com>
2021-05-31 18:28:12 -0700

    set up project ui and swifter library

---
+++ Twittermenti/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Twittermenti
+//
+//  Created by Cody Crawmer on 5/29/21.
+//
+
+import UIKit
+import SwifteriOS
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var sentimentLabel: UILabel!
+    
+    let swifter = Swifter(consumerKey: apiKey, consumerSecret: apiSecretKey)
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func predictPressed(_ sender: UIButton) {
+        
+    }
+    
+}
+
